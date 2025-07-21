bb8b125d8ee7de4d03febad0778c7d320e9bbaee
rodofrodo <rodofrodo708@gmail.com>
2025-07-21 12:34:10 +0200

    Enum classes

---
+++ app/src/main/java/com/example/easycalc/Input.kt
@@ -0,0 +1,5 @@
+package com.example.easycalc
+
+enum class Input {
+    NOT_OP, OP, EQUALS
+}
---
+++ app/src/main/java/com/example/easycalc/Operator.kt
@@ -0,0 +1,5 @@
+package com.example.easycalc
+
+enum class Operator {
+    PLUS, MINUS, MULTIPLY, DIVIDE, EMPTY
+}
